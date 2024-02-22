9a1ae66fb9bfb56833e6847e6d74029f95af43f4
rom4a <aroundthefog@gmail.com>
2024-02-23 01:52:56 +0400

    base implementation

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-from models import Person
+from random import randint
+
+from models import Person, Chif
+
+
+
+year = 0
+population = []
+while 5:
+    print('year: ' + str(year))
+    year += 1
+    population_growth = randint(0, 50)
+    
+    for i in range(population_growth):
+        new_person = Person(age=0)
+        population.append(new_person)
+    print(len(population) )
+
+
+
+
+
--- models.py
+++ models.py
@@ -1,3 +1,27 @@
+from random import randint
+import names
+
+
 class Person:
+    def __init__(self, age=0, gender=True, best_friend=None):
+        self.name = names.get_full_name()
+        self.age = age
+        self.gender = gender
+        self.best_friend = best_friend
+        self.alcohol = True
+
+    def die(self):
+        del self
+
+new_person1 = Person(age=15)
+new_person2 = Person(age=14, best_friend=new_person1)
+print(new_person1.name)
+print(randint(1, 666))
+
+new_person1.die()
+print(new_person2.name)
+
+
+class Chif(Person):
     """pass"""
     pass
